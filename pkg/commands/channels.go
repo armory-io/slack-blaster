4044acbb0dca85803069556f098e4528996f03b2
Akshay Dayal <akshay.dayal@armory.io>
2019-12-05 12:51:29 -0700

    Add support for getting shared channels

--- pkg/commands/channels.go
+++ pkg/commands/channels.go
@@ -6,28 +6,45 @@ import (
 
 	"github.com/nlopes/slack"
 	"github.com/ryanuber/go-glob"
-	"github.com/urfave/cli"
 )
 
-func Channels(c *cli.Context) error {
-	pattern := c.String("pattern")
-	slackToken := c.GlobalString("slack-token")
+func Channels(slackToken string, pattern string, sharedOnly bool) error {
 	api := slack.New(slackToken)
 
-	slackChannels, err := api.GetChannels(true)
+	slackChannels := []slack.Channel{}
+	types := []string{"public_channel"}
+	err := error(nil)
+	cursor := ""
 
-	if err != nil {
-		return err
+	// Loop through pagination till we get all slack channels.
+	for hasMorePages := true; hasMorePages; hasMorePages = !(cursor == "") {
+		slackChannelsPage := []slack.Channel{}
+		params := slack.GetConversationsParameters{cursor, "true", 1000, types}
+
+		slackChannelsPage, cursor, err = api.GetConversations(&params)
+
+		if err != nil {
+			return err
+		}
+
+		slackChannels = append(slackChannels, slackChannelsPage...)
 	}
 
 	names := []string{}
 
 	for _, c := range slackChannels {
-		if glob.Glob(pattern, c.Name) {
-			names = append(names, c.Name)
+		// If a pattern was specified ensure the channel name matches.
+		if !glob.Glob(pattern, c.Name) {
+			continue
 		}
+		// If looking for shared only channels check the property.
+		if sharedOnly && !c.IsShared {
+			continue
+		}
+
+		names = append(names, c.Name)
 	}
 
-	fmt.Printf("channels matching %s\n%s", pattern, strings.Join(names, "\n"))
+	fmt.Printf("channels matching %s\n%s\n", pattern, strings.Join(names, "\n"))
 	return nil
 }
